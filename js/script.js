bdf1f0f38b0f313bbad0c321c93e0e01d30e4894
Wander-XIV <aroy1420@gmail.com>
2022-03-12 12:05:01 -0500

    pokemonRepository
    Created an array containing 3 Pokémon that displays the name, height, weight, and type.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,47 @@
-alert('Hello world');
+// IIFE
+let pokemonRepository = (function () {
+  let pokemonList = [{
+    name: 'Charmander',
+    height: 0.6,
+    weight: 8.5,
+    type: ['fire']
+},
 
-let favoriteFood = 'Pizza';
-document.write(favoriteFood);
+{
+  name: 'Bulbasaur',
+  height: 0.7,
+  weight: 6.9,
+  type: ['grass', ' poison']
+},
+
+{
+  name: 'Squirtle',
+  height: 0.5,
+  weight: 9,
+  type: ['water']
+},
+
+];
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+pokemonRepository.getAll().forEach(function(pokemon){
+  document.write(pokemon.name + ": height: "+ pokemon.height + ", weight: "+ pokemon.weight + ", type: "+ pokemon.type);
+  document.write ("<br/>");
+});
+
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({ name: 'Dragonite' });
+console.log(pokemonRepository.getAll());
