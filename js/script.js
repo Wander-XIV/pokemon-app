5a6e1495ef3ac02f42f9662f9778923b58a30c38
Wander-XIV <aroy1420@gmail.com>
2022-03-18 16:48:18 -0400

    Update script.js
    Removed function getAll. Added line breaks for height, weight and type under each Pokémon name. Wrote for loop to check if any height is 1 or greater and write text if true. Changed Bulbasaur to Venusaur to fit criteria.

--- js/script.js
+++ js/script.js
@@ -1,47 +1,54 @@
 // IIFE
-let pokemonRepository = (function () {
-  let pokemonList = [{
-    name: 'Charmander',
+let pokemonList = [
+  {
+    name: "Charmander",
     height: 0.6,
     weight: 8.5,
-    type: ['fire']
-},
+    type: ["fire"]
+  },
 
-{
-  name: 'Bulbasaur',
-  height: 0.7,
-  weight: 6.9,
-  type: ['grass', ' poison']
-},
+  {
+    name: "Venusaur",
+    height: 6.07,
+    weight: 220.5,
+    type: ["grass", " poison"]
+  },
 
-{
-  name: 'Squirtle',
-  height: 0.5,
-  weight: 9,
-  type: ['water']
-},
-
+  {
+    name: "Squirtle",
+    height: 0.5,
+    weight: 9,
+    type: ["water"]
+  }
 ];
 
-  function add(pokemon) {
-    pokemonList.push(pokemon);
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height > 1) {
+    document.write(
+      pokemonList[i].name +
+        "<br>Height : (" +
+        pokemonList[i].height +
+        ")" +
+        " Wow that's big!<br>" +
+        "Weight : (" +
+        pokemonList[i].weight +
+        ")<br>" +
+        "Type : (" +
+        pokemonList[i].type +
+        ")<br>"
+    );
+  } else {
+    document.write(
+      pokemonList[i].name +
+        "<br>Height : (" +
+        pokemonList[i].height +
+        ")<br>" +
+        "Weight : (" +
+        pokemonList[i].weight +
+        ")<br>" +
+        "Type : (" +
+        pokemonList[i].type +
+        ")<br>"
+    );
   }
-
-  function getAll() {
-    return pokemonList;
-  }
-
-  return {
-    add: add,
-    getAll: getAll
-  };
-})();
-
-pokemonRepository.getAll().forEach(function(pokemon){
-  document.write(pokemon.name + ": height: "+ pokemon.height + ", weight: "+ pokemon.weight + ", type: "+ pokemon.type);
-  document.write ("<br/>");
-});
-
-console.log(pokemonRepository.getAll());
-pokemonRepository.add({ name: 'Dragonite' });
-console.log(pokemonRepository.getAll());
+}
